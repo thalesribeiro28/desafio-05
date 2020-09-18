6f389157af9916af8e6fe1c800e2d0ccf6fd2fd9
thalesribeiro28 <thales.rib@hotmail.com>
2020-09-18 13:27:17 -0300

    desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string;
+  value  : number;
+  type : 'income'  |  'outcome';
+}
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce(
+      (acumulador: Balance,  transaction: Transaction) => {
+      switch (transaction.type){
+        case "income":  
+            acumulador.income += transaction.value;
+            break;
+        case "outcome":
+            acumulador.outcome += transaction.value;
+            break;
+        default: 
+          break;
+      }
+   
+      return acumulador;
+      
+    },{
+       income : 0,
+       outcome: 0,
+       total : 0
+    });
+
+    const total  = income - outcome;
+
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type} : CreateTransactionDTO): Transaction {
+    const transactions = new Transaction({
+      title, 
+      value,
+      type
+    });
+
+    this.transactions.push(transactions);
+    return transactions;
   }
 }
 
